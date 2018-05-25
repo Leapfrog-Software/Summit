689032a55666fcbe1b5d6a4944e96a87da826cd5
Leapfrog-Software <Leapfrog1357>
2018-05-25 23:12:10 +0900

    [iOS] Push通知を実装

--- iOS/Summit/Summit/AppDelegate.swift
+++ iOS/Summit/Summit/AppDelegate.swift
@@ -7,11 +7,53 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        self.configurePush(application: application)
+        
+        return true
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        
+    }
+    
+    private func configurePush(application: UIApplication) {
+        
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        center.requestAuthorization(options: [.badge, .sound, .alert], completionHandler: { isGranted, _ in
+            if isGranted {
+                DispatchQueue.main.async {
+                    application.registerForRemoteNotifications()
+                }
+            }
+        })
+    }
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        
+        guard response.notification.request.trigger is UNPushNotificationTrigger else {
+            return
+        }
+        completionHandler()
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        
+        guard notification.request.trigger is UNPushNotificationTrigger else {
+            return
+        }
+        completionHandler([])
+    }
+}
