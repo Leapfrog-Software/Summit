af0b7286c637438aa3cd0a383a70de37e18ccb30
Leapfrog-Software <Leapfrog1357>
2018-05-26 17:40:41 +0900

    [Android] Base64処理を修正

--- Android/app/src/main/java/leapfrog_inc/summit/Function/Base64Utility.java
+++ Android/app/src/main/java/leapfrog_inc/summit/Function/Base64Utility.java
@@ -9,12 +9,12 @@ import android.util.Base64;
 public class Base64Utility {
 
     public static String encode(String string) {
-        String encoded = new String(Base64.encode(string.getBytes(), Base64.URL_SAFE | Base64.NO_WRAP));
+        String encoded = new String(Base64.encode(string.getBytes(), Base64.DEFAULT | Base64.NO_WRAP));
         return (encoded == null) ? "" : encoded;
     }
 
     public static String decode(String string) {
-        String decoded = new String(Base64.decode(string, Base64.URL_SAFE | Base64.NO_WRAP));
+        String decoded = new String(Base64.decode(string, Base64.DEFAULT | Base64.NO_WRAP));
         return (decoded == null) ? "" : decoded;
     }
 }
